0a9695f4968f3560de42f8c2e753652b57ead60f
Ramona Ridgewell <ramonaridgewell@gmail.com>
2022-05-07 13:21:08 -0700

    mysql api

---
+++ routes/invoices.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+
+// get invoices list
+router.get('/list', function(req, res) {
+  let sql = `SELECT * FROM invoices`;
+  db.query(sql, function(err, data, fields) {
+    if (err) throw err;
+    res.json({
+      status: 200,
+      data,
+      message: "Invoices list retrieved successfully"
+    })
+  })
+});
+
+// get invoice by invoice_number
+router.get('/:invoice_number', function(req, res) {
+  let sql = `SELECT * FROM invoices WHERE invoice_number='${req.params.invoice_number}'`;
+  db.query(sql, function(err, data, fields) {
+    if (err) throw err;
+    res.json({
+      status: 200,
+      data,
+      message: `Invoice ${data} retrieved successfully`
+    })
+  })
+});
+
+// create new invoice
+router.post('/new', function(req, res) {
+  let sql = `INSERT INTO invoices(title, description, amount) VALUES (?)`;
+  let values = [
+    req.body.title,
+    req.body.description,
+    req.body.amount
+  ];
+  db.query(sql, [values], function(err, data, fields) {
+    if (err) throw err;
+    res.json({
+      status: 200,
+      message: "New invoice added successfully"
+    })
+  })
+});
+
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,37 @@
+const path = require('path');
 const express = require("express");
+const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const invoicesRouter = require('../routes/invoices');
 
 const PORT = process.env.PORT || 3001;
 
+db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Th3R00t)fAll#v1l!sM0n3y',
+  database: 'gravitylegal'
+});
+
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get("/api", (req, res) => {
+  res.set('Content-Type', 'application/json');
   res.json({ message: "Hello from server!" });
+});
+
+app.use('/invoices', invoicesRouter);
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
