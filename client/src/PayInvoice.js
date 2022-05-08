0d73b0449021cde72d4a3fc2fb001fb64deb5d2c
Ramona Ridgewell <ramonaridgewell@gmail.com>
2022-05-07 19:46:41 -0700

    messing with pay invoice error states

--- client/src/PayInvoice.js
+++ client/src/PayInvoice.js
@@ -10,19 +10,58 @@ import {
 import './App.css';
 
 const PayInvoice = () => {
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+  const [labelValue, setLabelValue] = useState('Amount');
+  const [textValue, setTextValue] = useState(`Please enter amount up to ${labelValue}`)
+  const [textErrorState, setTextErrorState] = useState(false);
   const [invoice, setData] = useState([]);
   const { invoice_number, } = useParams()
   useEffect(() => {
     fetch(`/invoices/${invoice_number}`)
       .then((res) => res.json())
-      .then((data) => setData(data.data[0]))
-  }, [invoice_number])
+      .then((data) => {
+        const invoice = data.data[0];
+        setData(invoice);
+        if (invoice.amount > 0) {
+          setButtonDisabled(false);
+          setLabelValue(`$${invoice.amount}`)
+        };
+        if (invoice.amount === 0) {
+          setLabelValue('Invoice is paid');
+          setTextValue('Thank you for your payment');
+        }
+      });
+  }, [invoice_number]);
 
   const checkForContent = (content) => {
     return !(content && Object.keys(content).length === 0 && Object.getPrototypeOf(content) === Object.prototype)
+  };
+
+  const isNumeric = (str) => {
+    if (typeof str != "string") return false 
+    return !isNaN(str) && !isNaN(parseFloat(str))
   }
 
-  const contents = (
+  const validateAmount= (e) => {
+    const value = e.target.value;
+    if (isNumeric(value)) {
+      setTextErrorState(false);
+      setLabelValue(`$${invoice.amount}`);
+      setTextValue(`Please enter amount up to $${labelValue}`);  
+    }
+    if (!isNumeric(value)) {
+      setTextValue('Please enter a valid number');
+      setTextErrorState(true);
+      return
+    }
+    if (value > invoice.amount) {
+      setTextValue(`Please adjust value to less than or equal to ${invoice.amount}`);
+      setTextErrorState(true);
+      return
+    }
+  };
+
+  const contents = () => (
     <Box>
       <Typography variant='h6'>
         Invoice number: {invoice.invoice_number}
@@ -36,17 +75,32 @@ const PayInvoice = () => {
       <Typography variant='h6'>
         Amount: ${invoice.amount}
       </Typography>
-      <Button variant='contained' size='large' >
+      <Button
+        disabled={buttonDisabled}
+        variant='contained'
+        size='large'
+      >
         Pay
       </Button>
-      <TextField id="outlined-basic" label="Amount" variant="outlined" size='small' />
+      <TextField
+        id="amount field"
+        disabled={buttonDisabled}
+        error={textErrorState}
+        onChange={e => validateAmount(e)}
+        label={labelValue}
+        variant="outlined"
+        size='small'
+      />
+      <Typography>
+        {textValue}
+      </Typography>
     </Box>
   )
 
   return ( 
     <>
       <Container>
-        {invoice && checkForContent(invoice) ? contents : <p>"Loading..."</p>}
+        {invoice && checkForContent(invoice) ? contents() : <p>"Loading..."</p>}
       </Container>
     </>
   )
